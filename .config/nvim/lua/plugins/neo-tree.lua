1da34c5847329e5b9d0396c0d1a4a7fa8c0cf0d5
Cry999 <when.the.cry999@gmail.com>
2024-03-13 08:10:14 +0900

    fix: prevent neo-tree from opening files in edgy windows

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -34,7 +34,7 @@ return {
         },
       },
       sources = { "document_symbols", "filesystem", "buffers", "git_status" },
-      open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+      open_files_do_not_replace_types = { "terminal", "trouble", "qf", "edgy" }, -- when opening files, do not use windows containing these filetypes or buftypes
       sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
       default_component_configs = {
         container = {

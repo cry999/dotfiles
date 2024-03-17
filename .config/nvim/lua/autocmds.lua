1db9ea25c35aa8542275bbd2caaf996645e310f0
Cry999 <when.the.cry999@gmail.com>
2024-03-18 00:26:27 +0900

    rm: unused codes

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -16,22 +16,6 @@ local autocmds = {
       end
     end,
   },
-  -- lazy = {
-  --   event = "BufLeave",
-  --   pattern = "*",
-  --   callback = function()
-  --     local inactive_floating_wins = vim.fn.filter(vim.api.nvim_list_wins(), function(_, v)
-  --       local file_type = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(v), "filetype")
-  --
-  --       return vim.api.nvim_win_get_config(v).relative ~= ""
-  --           and v ~= vim.api.nvim_get_current_win()
-  --           and file_type == "lazy"
-  --     end)
-  --     for _, w in ipairs(inactive_floating_wins) do
-  --       pcall(vim.api.nvim_win_close, w, false)
-  --     end
-  --   end,
-  -- },
 }
 
 for group, def in pairs(autocmds) do

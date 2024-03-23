315915214f9a6581c4dd4cecd8addc5b979c7b99
Cry999 <when.the.cry999@gmail.com>
2024-03-23 21:24:50 +0900

    chore(ui): bottom

--- .config/nvim/lua/plugins/nvim-notify.lua
+++ .config/nvim/lua/plugins/nvim-notify.lua
@@ -1,4 +1,25 @@
 return {
   "rcarriga/nvim-notify",
   init = function() vim.notify = require("notify") end,
+  opts = {
+    background_colour = "NotifyBackground",
+    fps = 30,
+    icons = {
+      DEBUG = "",
+      ERROR = "",
+      INFO = "",
+      TRACE = "✎",
+      WARN = ""
+    },
+    level = 2,
+    minimum_width = 50,
+    render = "wrapped-compact",
+    stages = "fade_in_slide_out",
+    time_formats = {
+      notification = "%T",
+      notification_history = "%FT%T"
+    },
+    timeout = 2000,
+    top_down = false,
+  },
 }

829aed926eb9d022686284b2157196176e756004
Cry999 <when.the.cry999@gmail.com>
2024-03-29 01:28:30 +0900

    chore(neovim-ui): improve actions-preview layout

--- nvim/.config/nvim/lua/plugins/actions-preview.lua
+++ nvim/.config/nvim/lua/plugins/actions-preview.lua
@@ -9,6 +9,13 @@ return {
       highlight_command = {
         require("actions-preview.highlight").delta(),
       },
+      telescope = {
+        layout_strategy = "vertical",
+        layout_config = {
+          width = 0.8,
+          height = 0.9,
+        },
+      },
     })
   end,
 }

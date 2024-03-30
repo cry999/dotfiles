8577558609ad37cf81747564c3b6105ac9545559
Cry999 <when.the.cry999@gmail.com>
2024-03-31 04:34:45 +0900

    chore(zsh): use $CATPPUCCIN_FLAVOUR instead of constant flavour

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -25,7 +25,7 @@ export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export GIT_EDITR=nvim
 export EDITOR=nvim
 
-export EZA_COLORS="$(vivid generate catppuccin-frappe)" # default styles
+export EZA_COLORS="$(vivid generate catppuccin-$CATPPUCCIN_FLAVOUR)" # default styles
 export EZA_COLORS="${EZA_COLORS}:da=0;38;2;140;170;238" # file's date
 export EZA_COLORS="${EZA_COLORS}:uu=0;38;2;229;200;144" # a user that's you
 export EZA_COLORS="${EZA_COLORS}:uR=0;38;2;231;130;132" # a user that's root

d96688097d40b62ccbc79505aa8b6a30cf4a9079
Cry999 <when.the.cry999@gmail.com>
2024-04-05 12:48:47 +0900

    chore(neovim-ui): change flavour to pink-mocha

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -6,7 +6,7 @@ return {
     local U = require("catppuccin.utils.colors")
     local tp = false
     require("catppuccin").setup({
-      flavour = vim.env.CATPPUCCIN_FLAVOUR or "mocha",
+      flavour = "mocha",
       transparent_background = tp,
       integrations = {
         aerial = true,
@@ -104,6 +104,23 @@ return {
           FoldCol8 = { fg = c.text, bg = U.blend(c.base, c.text, 0.55) },
         }
       end,
+      color_overrides = {
+        mocha = {
+          text = "#F4CDE9",
+          subtext1 = "#DEBAD4",
+          subtext0 = "#C8A6BE",
+          overlay2 = "#B293A8",
+          overlay1 = "#9C7F92",
+          overlay0 = "#866C7D",
+          surface2 = "#705867",
+          surface1 = "#5A4551",
+          surface0 = "#44313B",
+
+          base = "#352939",
+          mantle = "#211924",
+          crust = "#1a1016",
+        },
+      },
     })
     vim.cmd.colorscheme("catppuccin")
   end,

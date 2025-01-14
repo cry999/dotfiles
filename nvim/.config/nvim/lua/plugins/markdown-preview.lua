8257966d0b2e53e2b3266b84d1cc856c198057cb
Cry999 <when.the.cry999@gmail.com>
2025-01-15 06:11:15 +0900

    chore(neovim): remove unused 'markdown-preview' package

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-}
